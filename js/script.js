111ee3ff8f43b031c36cd85e45e6b8fe7ad4b450
mercedes-arnwine <80288739+mercedes-arnwine@users.noreply.github.com>
2023-01-14 20:10:23 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var firstListItem = document.querySelector("ul li");
+//console.log(firstListItem);
+
+firstListItem.style.textTransform = "uppercase";
+
+var moreReasons = document.querySelector(".more-reasons");
+//console.log(moreReasons);
+
+moreReasons.style.fontSize = "2.5em";
+
+var whyJS = document.querySelector("h3");
+console.log(whyJS);
+
+whyJS.innerHTML = 'Why learn <span class= "highlight"> Javascript </span>?';
+
+var highlight = document.querySelector(".highlight");
+highlight.style.backgroundColor = "#ffff82";
+
+var mainImage = document.querySelector("img");
+console.log(mainImage);
+
+//mainImage.src = img/js-code.png;
+//mainImage.alt = example;
